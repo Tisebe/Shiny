58a89687ed4bc6613404a88403fdefe0da27fe24
Tony Isebe <48394241+Tisebe@users.noreply.github.com>
2020-03-24 22:10:52 +0300

    more on shiny

--- my_shiny.R
+++ my_shiny.R
@@ -33,3 +33,41 @@ server <- function(input, ouput){
   
   
 shinyApp(ui=ui, server = server)
+
+
+
+library(shiny)
+
+
+#Define UI
+
+ui <- fluidPage(
+  titlePanel('My title'),
+  sidebarLayout(
+    sidebarPanel('Sidebar Panel'),
+    mainPanel(
+      p('p creates a paragraph text'),
+      strong('strong() makes bold text'),
+      em('em() creates italicized text'),
+      code('code displays your text similar to computer code'),
+      div('creates segments of text with a similar style'),
+      h6('Sixth level title')
+    )
+  )
+)
+
+
+
+#Define server logic
+
+
+server <- function(input, ouput){
+  
+}
+
+
+
+#Run the app
+
+
+shinyApp(ui=ui, server = server)
